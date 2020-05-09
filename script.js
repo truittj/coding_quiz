5b097ffed802146bceb1c8f2a02212907043d8be
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-09 17:05:05 -0500

    quiz page randomizer has a functioning random instance to grab btn. Will use this for random answer.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-console.log();
 var timerEl = document.getElementById("timer");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var btn3 = document.getElementById("btn3");
+var btn4 = document.getElementById("btn4");
 
 var secondsLeft = 60;
 
@@ -10,8 +13,12 @@ function setTime() {
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      //sendMessage();
+      //sendMessage(); $$add some function here that will move to table page with store high score values
     }
   }, 1000);
 }
-setTime();
+//setTime();
+btnArray = [btn1, btn2, btn3, btn4];
+var btnRandom = btnArray[Math.floor(Math.random() * btnArray.length)];
+console.log(btnRandom);
+console.log(btnArray);
