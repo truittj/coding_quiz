498acbc594065ca53cca283f403bed8a5cf38da5
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-10 12:45:28 -0500

    Up all night and have marginal functionality. The timer isn't working now, but the start btn is.

--- questions.js
+++ questions.js
@@ -1,20 +1,30 @@
 //going to use functions to reference this sheet. I think
 
 var questions = [
-    {title: "Inside which HTML element do we put the JavaScript?"
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
     choices: ["<javascript>", "<scripting>", "<js>", "<script>"],
     answer: "<script>",
-},
+  },
 
-{
-    
+  {
     title: "What is a DOM in JavaScript?",
-    choices: ["Data of Mine", "Document of Master", "Data Object Modal", "Document Object Model"],
+    choices: [
+      "Data of Mine",
+      "Document of Master",
+      "Data Object Modal",
+      "Document Object Model",
+    ],
     answer: "Document Object Model",
-},
-    {
+  },
+  {
     title: "Where is the correct place to insert a JavaScript?",
-    choices: ["The <head> section", "The <body> section", "Both the <head> section and the <body> section are correct", "Neither are correct"],
+    choices: [
+      "The <head> section",
+      "The <body> section",
+      "Both the <head> section and the <body> section are correct",
+      "Neither are correct",
+    ],
     answer: "Both the <head> section and the <body> section are correct",
-},
+  },
 ];
--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 var timerEl = document.getElementById("timer");
-var btn1 = document.getElementById("btn1");
+//removed buttons becuase it was easier to work with li's
+/*var btn1 = document.getElementById("btn1");
 var btn2 = document.getElementById("btn2");
 var btn3 = document.getElementById("btn3");
-var btn4 = document.getElementById("btn4");
-var submitBtn = document.querySelector("button.submitBtn");
-var submitScoreEl = document.querySelector("#submit-score");
-var userScoreEl = document.getElementById("user-score");
+var btn4 = document.getElementById("btn4");*/
+var landText = document.getElementById("landingText");
+var showQuestons = document.getElementById("showQuestons");
+var startQuizBtn = document.getElementById("startQuizBtn");
+var submitBtn = document.getElementById("submitBtn");
+var submitScoreEl = document.getElementById("submitScore");
+var userScoreEl = document.getElementById("userScore");
 var userNameInput;
 var questionHeader = document.getElementById("question1");
 var answerChoices = document.getElementById("answers");
-var questionNumber = -1;
-var secondsLeft = 60 + 1;
+var secondsLeft = 60;
 var answer;
+
+startQuizBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  // swap welcome msg w/ questions
+  landText.classList.add("d-none");
+
+  showQuestons.classList.remove("d-none");
+
+  // timer set and begins 60s
+}
 
 function setTime() {
   var timerInterval = setInterval(function () {
@@ -28,82 +42,4 @@ function setTime() {
 }
 //setTime();
 
-// too shuffle btn array, to over write after onClick happens
-
-function makeQuestions() {
-  questionNumber++;
-  answer = questions[questionNumber].answer;
-
-  questionHeader.textContent = questions[questionNumber].title;
-  answerChoices.innerHTML = "";
-
-  var choices = questions[questionNumber].choices;
-
-  for (var q = 0; q < choices.length; q++) {
-    var nextChoice = document.createElement("button");
-
-    nextChoice.textContent = choices[q];
-    answerBtn = answerChoices
-      .appendChild(nextChoice)
-      .setAttribute("class", "p-3 m-1 btn btn-primary btn-block");
-  }
-}
-// display option to enter name to scoreboard
-function displayScore() {
-  document.getElementById("quiz").classList.add("d-none");
-  document.getElementById("submit-score").classList.remove("d-none");
-  userScoreEl.textContent = "Your final score is " + secondsLeft + ".";
-}
-
-// Event Listeners for Main Buttons
-startBtn.addEventListener("click", startTimer);
-submitBtn.addEventListener("click", function (event) {
-  event.stopPropagation();
-  addScore();
-
-  window.location.href = "./highscores.html";
-});
-
-function addScore() {
-  userNameInput = document.getElementById("userName").value;
-
-  // create a new object with name and score keys
-  var newScore = {
-    name: userNameInput,
-    score: secondsLeft,
-  };
-  // check if there are scores in local storage first(get it)
-  //if not, make a new/blank array
-  var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
-  // push object into score array
-  highScores.push(newScore);
-  // turn objects into an array of strings then put it into local storage
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-}
-
-function hideFeedback() {
-  var pEl = document.getElementsByClassName("feedback")[0];
-  pEl.style.display = "none";
-}
-
-function showFeedback() {
-  var pEl = document.getElementsByClassName("feedback")[0];
-  pEl.removeAttribute("style");
-}
-
-answerChoices.addEventListener("click", function (event) {
-  var pEl = document.getElementsByClassName("feedback")[0];
-
-  // evaluation of user's answer choices & feedback
-  if (answer === event.target.textContent) {
-    pEl.innerHTML = "Correct!";
-    setTimeout(hideFeedback, 1000);
-    showFeedback();
-  } else {
-    pEl.innerHTML = "Sorry, that's incorrect.";
-    setTimeout(hideFeedback, 1000);
-    secondsLeft = secondsLeft - 10;
-    showFeedback();
-  }
-  makeQuestions();
-});
+// too shuffle li array, to over write after onClick happens
