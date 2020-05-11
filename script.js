678fa01f3a6ce630b9b8d9495cb4201b2325d2c4
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-11 18:08:53 -0500

    i think it is all working. i amgoing to check with my tutor.

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function setTime() {
     secondsLeft--;
     timerEl.textContent = "Time:" + secondsLeft;
 
-    if (secondsLeft === 0) {
+    if (secondsLeft === 0 || questionNumber === questions.length) {
       clearInterval(timerInterval);
       setTimeout(displayScore, 500);
 
@@ -64,3 +64,62 @@ function makeQuestions() {
       .setAttribute("class", "btn btn-primary btn-block");
   }
 }
+// display user name to scoreboard
+function displayScore() {
+  document.getElementById("quiz").classList.add("d-none");
+  document.getElementById("submitScore").classList.remove("d-none");
+  userScoreEl.textContent = "Your final score is " + secondsLeft + ".";
+}
+
+// Event Listeners for Main Buttons
+startQuizBtn.addEventListener("click", startQuiz);
+submitBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  addScore();
+
+  window.location.href = "./highscores.html";
+});
+
+function addScore() {
+  userNameInput = document.getElementById("userName").value;
+
+  // create a new object with name and score keys
+  var newScore = {
+    name: userNameInput,
+    score: secondsLeft,
+  };
+  // check if there are scores in local storage first(get it)
+  //if not, make a new/blank array
+  var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+  // push object into score array
+  highScores.push(newScore);
+  // turn objects into an array of strings then put it into local storage
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+function hideFeedback() {
+  var pEl = document.getElementsByClassName("accuracy")[0];
+  pEl.style.display = "none";
+}
+
+function showFeedback() {
+  var pEl = document.getElementsByClassName("accuracy")[0];
+  pEl.removeAttribute("style");
+}
+
+answerChoices.addEventListener("click", function (event) {
+  var pEl = document.getElementsByClassName("accuracy")[0];
+
+  // evaluation of user's answer choices & feedback
+  if (answer === event.target.textContent) {
+    pEl.innerHTML = "Correct!";
+    setTimeout(hideFeedback, 1000);
+    showFeedback();
+  } else {
+    pEl.innerHTML = "Sorry, that's incorrect.";
+    setTimeout(hideFeedback, 1000);
+    secondsLeft = secondsLeft - 10;
+    showFeedback();
+  }
+  makeQuestions();
+});
